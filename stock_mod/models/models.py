1abfe02fae40830090a0b57fad4de2eb5e569483
soytrillos <trillos2000@gmail.com>
2022-02-16 16:42:32 -0500

    addons responsive y refresh

---
+++ stock_mod/models/models.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class stock_mod(models.Model):
+#     _name = 'stock_mod.stock_mod'
+#     _description = 'stock_mod.stock_mod'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+
+from odoo import models, fields, api
+
+class StockPickinInherit(models.Model):
+    _inherit = "stock.picking"
+ 
+    city = fields.Char(string='Ciudad cliente', related='partner_id.city', store=True)
+    street = fields.Char(string='Direccion cliente', related='partner_id.street', store=True)
